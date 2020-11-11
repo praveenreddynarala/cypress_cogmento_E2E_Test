95b6ae6a74f1ba48352993b52c5aaada1977cc73
Praveen Reddy Narala <praveenreddy.narala@gmail.com>
2020-11-11 22:24:36 +0000

    Update jenkinsfile.groovy

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -1,5 +1,5 @@
 pipeline {
-  agent any
+    agent any
 //   agent {
     // this image provides everything needed to run Cypress
     // docker {
@@ -16,17 +16,16 @@ pipeline {
         // http://localhost:8080/pipeline-syntax/globals#env
         echo "Running build ${env.BUILD_ID} on ${env.JENKINS_URL}"
         sh 'npm ci'
-        sh 'npm run cy:verify'
       }
     }
 
-    stage('start local server') {
-      steps {
-        // start local server in the background
-        // we will shut it down in "post" command block
-        sh 'npm run start:ci &'
-      }
-    }
+    // stage('start local server') {
+    //   steps {
+    //     // start local server in the background
+    //     // we will shut it down in "post" command block
+    //     sh 'nohup npm run start:ci &'
+    //   }
+    // }
 
     // this stage runs end-to-end tests, and each agent uses the workspace
     // from the previous stage
