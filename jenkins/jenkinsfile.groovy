84cf94d41d90ebce6bae50ff4a4b8de5b6d8af2b
Narala <praveenreddy.narala@capgemini.com>
2020-11-12 11:07:59 +0000

    Removing all files in .gitignore

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -1,10 +1,11 @@
 pipeline {
-  agent {
+    agent any
+//   agent {
     // this image provides everything needed to run Cypress
-    docker {
-      image 'cypress/base:10'
-    }
-  }
+    // docker {
+    //   image 'cypress/base:10'
+    // }
+//   }
 
   stages {
     // first stage installs node dependencies and Cypress binary
@@ -15,17 +16,16 @@ pipeline {
         // http://localhost:8080/pipeline-syntax/globals#env
         echo "Running build ${env.BUILD_ID} on ${env.JENKINS_URL}"
         sh 'npm ci'
-        sh 'npm run cy:verify'
       }
     }
 
-    stage('start local server') {
-      steps {
-        // start local server in the background
-        // we will shut it down in "post" command block
-        sh 'nohup npm run start:ci &'
-      }
-    }
+    // stage('start local server') {
+    //   steps {
+    //     // start local server in the background
+    //     // we will shut it down in "post" command block
+    //     sh 'nohup npm run start:ci &'
+    //   }
+    // }
 
     // this stage runs end-to-end tests, and each agent uses the workspace
     // from the previous stage
